7d357427f74ac71f2284482c3a3de62b07d0b58a
Jonas Cabral <5769555+Truleu@users.noreply.github.com>
2023-12-05 22:32:15 -0300

    Attempt to do something on an old dumb Mac. Couldn't debug

--- Day2/PartOne.cs
+++ Day2/PartOne.cs
@@ -4,6 +4,20 @@ public static class PartOne
   //Bag loaded with only 12 red cubes, 13 green cubes and 14 blue cubes
   public static int PossibleGamesSum(IEnumerable<string> games)
   {
-    
+    List<int> numbers = new();
+    Game gameObj = new();
+    foreach (var game in games)
+    {
+      var gameSplit = game.Split(':');
+      gameObj.GameId = int.Parse(gameSplit[0].Split(' ')[1]);
+
+      List<string> handful = new();
+      handful.AddRange(gameSplit[1].Split(';'));
+      var handfulOfRandomCubes = gameSplit[1].Split(';');
+      for (var i = 0; i < handfulOfRandomCubes.Length; i++){
+        var cubes = handfulOfRandomCubes[i].Split(',');
+    }
+  }
+    return 0;
   }
 }
